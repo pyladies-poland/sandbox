54d8338e41f6a27e232be3a32ab5500303c098b9
Piotr Dyba <piotr@dyba.com.pl>
2015-01-07 18:53:51 +0100

    show map using address and generates cordinates or future use

--- places/models.py
+++ places/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from easy_maps.geocode import google_v3 as geocode
 
+
 class Place(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=450)
@@ -11,18 +12,27 @@ class Place(models.Model):
     address_post_code = models.CharField(max_length=8)
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    latitude = models.FloatField(max_length=16, blank=True)
-    longitude = models.FloatField(max_length=16, blank=True)
-
+    latitude = models.DecimalField(max_digits=16, blank=True,
+                                   decimal_places=8)
+    longitude = models.DecimalField(max_digits=16, blank=True,
+                                    decimal_places=8)
 
     def __unicode__(self):
         return self.name
 
-    def full_address(self):
-        return unicode(self.address_country) + ", " + unicode(self.address_city) + \
+    def get_full_address(self):
+        """
+        create string with full address
+        """
+        return unicode(self.address_country) + \
+               ", " + unicode(self.address_city) + \
                ", " + unicode(self.address_street)
 
-    def geocode(self):
-        new_geocode = geocode(self.full_address())
-        self.latitude = new_geocode[0]
+    def fill_geocode(self):
+        new_geocode = geocode(self.get_full_address())
+        self.latitude = new_geocode[0]  #
         self.longitude = new_geocode[1]
+
+    def save(self, *args, **kwargs):
+        self.fill_geocode()
+        super(Place, self).save(*args, **kwargs)
