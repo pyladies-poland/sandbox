b953d040d66af4fb364f828f73464ce12129b245
dorotawm <wrobel.d.h@gmail.com>
2015-02-04 19:16:08 +0000

    [#37] - Added simple home page, templates folder and modify settings and main urls

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -105,6 +105,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'accounts/templates'), )
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'accounts/templates'),
+os.path.join(BASE_DIR, 'templates')
+)
 
 AUTH_USER_MODEL = 'accounts.User'
--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"), 
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^places/', include('places.urls', namespace='places'))
 )
