afc39237a7413ea60b178d18787954bea9612444
dorotawm <wrobel.d.h@gmail.com>
2015-03-26 20:02:06 +0000

    [#37] Modified urls.py - apostrophe

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"), 
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'), 
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^places/', include('places.urls', namespace='places'))
 )
